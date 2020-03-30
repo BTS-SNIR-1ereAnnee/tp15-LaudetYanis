72d3ba7c78bf2c1977543a15c999fa1f513b6237
C. Tonnerre <26811017+ctonnerre@users.noreply.github.com>
2020-03-30 10:30:56 +0200

    ajout du squelette JavaScript pour lire le fichier JSON

---
+++ lectureJSON.js
@@ -0,0 +1,21 @@
+xhttp.onreadystatechange = function() {
+    //teste si les codes retour sont OK = on a bien lu le fichier demande
+    if (this.readyState == 4 && this.status == 200) {
+      // lit la premiere ligne du fichier et la met dans maLigne
+        var maLigne = this.responseText;
+            // contient un seul objet JSON de type
+            //{"hum":"47.61 ","temp":" +21.00 ","pression":"996.15 ","timestamp":1585494629661}
+            console.log(maLigne);
+            try{
+              // parse le JSON = recupere l'objet au format JSON
+              data = JSON.parse(maLigne);
+            }catch (e) {
+              console.error("Erreur d'analyse du JSON :", e);
+            }
+
+
+        }
+
+};
+xhttp.open("GET", "cheminEtFichier.json", true);
+xhttp.send();
